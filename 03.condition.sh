e5316049226850f4be4a638e48c6cc6c361f1a4b
praveen <praveensunnam1221@gmail.com>
2025-09-22 15:44:30 +0530

    corrected conds

--- 03.condition.sh
+++ 03.condition.sh
@@ -5,7 +5,7 @@ NUM=$1
 if [ $NUM -lt 20 ]; then
     echo "given number $NUM is less than 10"
 elif [ $NUM -eq 20 ];
-    echo "given number $NUM is equal to $Num"   
+    echo "given number $NUM is equal to $NUM"   
 else
     echo "given number $NUM is greater than 10"              
 fi    
