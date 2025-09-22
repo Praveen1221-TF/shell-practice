270640145c21d5004d01a129caf6ccbaa9a03898
praveen <praveensunnam1221@gmail.com>
2025-09-22 15:41:47 +0530

    corrected conds

--- 03.condition.sh
+++ 03.condition.sh
@@ -4,9 +4,9 @@ NUM=$1
 
 if [ $NUM -lt 20 ]; then
     echo "given number $NUM is less than 10"
-elif
-    echo "given number $NUM is greater than 10"
+elif [ $NUM -eq 20 ];
+    echo "given number $NUM is equal to $Num"   
 else
-    echo "given number $NUM is equal to $Num"                   
+    echo "given number $NUM is greater than 10"              
 fi    
 
