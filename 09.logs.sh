f2d503c7e401c4aad33c4f8e112f3b218b80a484
praveen <praveensunnam1221@gmail.com>
2025-09-24 15:59:27 +0530

    logs

---
+++ 09.logs.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$( echo $0 | cut -d "." f1,2 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p LOGS_FOLDER
+echo "script started excuted at : $(date)"
+
+if [ $USERID -ne 0 ]; then        
+    echo "Error: please run the script with root user"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$R Error: install $2 is failure $N"
+         exit 1
+    else        
+        echo -e "$G installing $2 is success $N"
+
+    fi
+
+}
+
+dnf list install mysql &>>LOG_FILE
+if [ $? -ne 0 ]; then
+    dnf install mysql -y &>>LOG_FILE
+    VALIDATE $? "MYSQL"
+else
+    echo -e " mysql already exists..$G skipping $N"  
+fi
+
+dnf list install nginx -y &>>LOG_FILE
+if [ $? -ne 0 ]; then
+    dnf install nginx -y &>>LOG_FILE
+    VALIDATE $? "nginx"
+else
+    echo -e "nginx already exists...$Y skipping $N"  
+fi     
+
+
+dnf list install python3 -y &>>LOG_FILE   
+if [ $? -ne 0 ]; then
+    dnf install python3 -y &>>LOG_FILE
+    VALIDATE $? "python3"
+else
+    echo -e "PYTHON3 already exists..$R skipping $N"  
+fi     
+
