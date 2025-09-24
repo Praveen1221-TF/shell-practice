22efd950b41107dd319ca44e9f93dec8b304d123
praveen <praveensunnam1221@gmail.com>
2025-09-24 12:28:56 +0530

    installation changes

--- 06.installation.sh
+++ 06.installation.sh
@@ -3,15 +3,38 @@
 USERID=$(id -u) #user ID
 
 if [$USERID -ne 0]; then        
-    echo "Error: please run the script"
+    echo "Error: please run the script with root user"
+    exit 1
 fi
 
 
-dnf install mysql -y        
+
+dnf install mysql -y         
 
 if [ $? -ne 0 ]; then
     echo "Error: install mysql is failure"
+    exit 1
 else        
     echo "installing mysql is success"
+
+fi
+
+dnf install nginx -y         
+
+if [ $? -ne 0 ]; then
+    echo "Error: install nginx is failure"
+    exit 1
+else        
+    echo "installing nginx is success"
+
+fi
+
+dnf install mongodb-mongosh -y      
+
+if [ $? -ne 0 ]; then
+    echo "Error: install mysmongodb-mongosh is failure"
+    exit 1
+else        
+    echo "installing mmongodb-mongosh is success"
 
 fi
