072bd4d7cd3c3d565dd6c1dc4110657d7aa803b1
praveen <praveensunnam1221@gmail.com>
2025-09-24 14:49:35 +0530

    installation with colors

---
+++ 08.inc.sh
@@ -0,0 +1,47 @@
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u) #user ID
+
+if [ $USERID -ne 0 ]; then        
+    echo "Error: please run the script with root user"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$R Error: install $2 is failure $N"
+         exit 1
+    else        
+        echo -e "$G installing $2 is success $N"
+
+    fi
+
+}
+
+dnf list install mysql
+if [ $? -ne 0 ]; then
+    dnf install mysql -y
+    VALIDATE $? "MYSQL"
+else
+    echo -e "$G mysql already exists.. skipping $N"  
+fi
+
+dnf list install nginx -y 
+if [ $? -ne 0 ]; then
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else
+    echo -e "$Y nginx already exists.. skipping $N"  
+fi     
+
+
+dnf list install python3 -y      
+if [ $? -ne 0 ]; then
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else
+    echo -e "$R nginx already exists.. skipping $N"  
+fi     
