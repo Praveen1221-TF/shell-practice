39cf541165d10e0bc380d688dea4884f3f4bd2f8
praveen <praveensunnam1221@gmail.com>
2025-09-27 10:57:38 +0530

    chnages done

--- 09.logs.sh
+++ 09.logs.sh
@@ -6,7 +6,7 @@ N="\e[0m"
 
 USERID=$(id -u)
 
-LOGS_FOLDER="/var/log/shellscript-logs"
+LOGS_FOLDER= "/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1,2 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
