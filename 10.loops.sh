0f563ad8d31fe77447f6b321148f6e93516f9918
praveen <praveensunnam1221@gmail.com>
2025-09-27 15:27:22 +0530

    loops

---
+++ 10.loops.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(basename "$0" .sh)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" 
+
+mkdir -p $LOGS_FOLDER
+echo "script started excuted at : $(date)"
+
+if [ $USERID -ne 0 ]; then        
+    echo "Error: please run the script with root user"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$R Error: install $2 is failure $N" | tee -a $LOG_FILE
+         exit 1
+    else        
+        echo -e "$G installing $2 is success $N" | tee -a $LOG_FILE
+
+    fi
+}   
+
+for package in $@
+do  
+    echo "package is : $package
+done    
